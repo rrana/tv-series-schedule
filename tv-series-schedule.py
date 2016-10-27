11749a5813fc8d89161bdda6a39c2fcae1bdaf93
rrana <rana@newscred.com>
2016-10-27 18:33:12 +0600

    Remove hardcoded tv series name, imdb id, and season mapping. Using OMDB api to fetch informations.

--- tv-series-schedule.py
+++ tv-series-schedule.py
@@ -7,6 +7,30 @@ from datetime import datetime
 import calendar
 
 
+def get_omdb_result(tv_series_name):
+    omdb_url = 'http://www.omdbapi.com/'
+
+    params = {
+        't': tv_series_name,
+    }
+
+    api_response = requests.get(url=omdb_url, params=params)
+    return api_response
+
+def validate_omdb_result(api_response):
+    if api_response.json()['Response'] == 'False':
+        print "Invalid name! Please check name again!"
+        return
+    elif api_response.json()['Type'] != 'series':
+        print "Not tv series. Found match for %s instead" % api_response.json()['Type']
+        return
+    else:
+        return api_response
+
+def get_current_season(api_response):
+    current_season = api_response.json()['totalSeasons']
+    return current_season
+
 def date_in_imdb_format():
     now = datetime.now()
     current_date = now.day
@@ -15,53 +39,36 @@ def date_in_imdb_format():
     imdb_formatted_date = str(current_date) + " " + str(current_month) + " " + str(current_year)
     return imdb_formatted_date
 
-
 def get_episode_information(args):
     #todo: suggest tv series name if no match found. Considering possibilites of spelling mistake
+    #todo: handle exception in case tv series name is invalid.
     tv_series_name = args.series_name.lower()
+    omdb_result =  get_omdb_result(tv_series_name)
 
-    #todo: get this mapping using api
-    tv_series_mapping = {
-                            "the big bang theory":"tt0898266", 
-                            "the middle":"tt144246", 
-                            "modern family":"tt1442437", 
-                            "fresh off the boat":"tt3551096",
-                            "shameless":"tt1586680",
-                        }
+    if validate_omdb_result(omdb_result):
+        tv_series_id = omdb_result.json()['imdbID']
+        current_season = omdb_result.json()['totalSeasons']
+        imdb_request_url = "http://www.imdb.com/title/" + tv_series_id + "/episodes?season=" + current_season
 
-    #todo: get this mapping using api
-    current_season_mapping = {
-                        "tt0898266":10, 
-                        "tt144246":8, 
-                        "tt1442437":8, 
-                        "tt3551096":3,
-                        "tt1586680":7,
-                    }
+        imdb_response = requests.get(url=imdb_request_url)
+        html_content = imdb_response.content
+        soup = BeautifulSoup(html_content)
 
-    tv_series_id = tv_series_mapping[tv_series_name]
-    current_season = str(current_season_mapping[tv_series_id])
+        air_date = []
 
-    imdb_request_url = "http://www.imdb.com/title/" + tv_series_id + "/episodes?season=" + current_season
+        for episodes in soup.findAll('div', attrs={'class':'airdate'}):
+             air_date.append(episodes.getText())
 
-    imdb_response = requests.get(url=imdb_request_url)
-    html_content = imdb_response.content
-    soup = BeautifulSoup(html_content)
-
-    air_date = []
-
-    for episodes in soup.findAll('div', attrs={'class':'airdate'}):
-         air_date.append(episodes.getText())
-
-    today = str(date_in_imdb_format())
+        today = str(date_in_imdb_format())
     
-    if today in air_date:
-        print "Found one episode of %s scheduled today: %s" % (args.series_name, today)
-    else:
-        #todo: print next episodes schedule
-        print "Could not find any episode scheduled for: %s" % today
-        print "Schedule for this season: \n"
-        for dates in air_date:
-            print dates
+        if today in air_date:
+            print "Found one episode of %s scheduled today: %s" % (args.series_name, today)
+        else:
+            #todo: print next episodes schedule
+            print "Could not find any episode scheduled for: %s" % today
+            print "Schedule for this season: \n"
+            for dates in air_date:
+                print dates
 
 
 if __name__ == "__main__":    
